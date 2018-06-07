e3dd2eb572641f9086ddb24b232f8eed2e0366dc
popkim <kimlyb@163.com>
2018-06-07 21:24:35 +0800

    基本Constructor

---
+++ Constructor.js
@@ -0,0 +1,15 @@
+function  Car(model, year, miles) {
+    this.model = model;
+    this.year = year;
+    this.miles = miles;
+
+    this.toString = function () {
+        return this.model + ' has done ' + this.miles + ' miles from ' + this.year;
+    }
+}
+
+var lanniao = new Car("Nissan LANNIAO", 2015, 20000);
+var mondeo = new Car("Ford Mondeo", 2017, 5000);
+
+console.log(lanniao.toString());
+console.log(mondeo.toString());
